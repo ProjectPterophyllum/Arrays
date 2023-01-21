ac5a1c91b5b313335ea33eb4a90b3ca42c60abce
Alexey Bolshakov <bad2600@outlook.com>
2023-01-21 23:00:44 +0300

    UPD

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -8,45 +8,70 @@ void enterarraywithkeyboard(int array[], int N)
 	{
 		cin >> array[i];
 	}
-}
+}//Заполнение массива с помощью ввода с клавиатуры
 void fillarray(int array[], int N)
 {
 	for (int i = 0; i < N; i++)
 		array[i] = i;
-}
+}//Заполнение массива числами от 0 до N
 void coutarray(int array[], int N)
 {
 	for (int i = 0; i < N; i++)
 		cout << array[i] << " ";
 	cout << endl;
-}
+} //Вывод массива в прямом последовательности
 void coutarrayrev(int array[], int N)
 {
 	for (int i = N - 1; i >= 0; i--)
 	{
 		cout << array[i] << " ";
 	}
-}
+} //Вывод массива в обратном последовательности
 int randomize(int start, int end)
 {
 	int random = 0;
 	random = start + rand() % (end - start + 1);
 	return random;
-}
+} //Случайное значение переменной в диапазоне от и до
 void randomfillarray(int start, int end, int array[], int N)
 {
 	for (int i = 0; i < N; i++)
 		array[i] = randomize(start, end);
-}
-int summelementsofarray(int array[], int N)
+} //Заполнение массива случайными числами
+float summelementsofarray(int array[], int N)
 {
-	int sum = 0;
+	float sum = 0;
 	for (int i = 0; i < N; i++)
 	{
 		sum += array[i];
 	}
 	return sum;
-}
+} //Сумма элементов массива
+float middleariphmetical(int array[], int N)
+{
+	float Mar = 0;
+	return Mar = summelementsofarray(array, N) / N;
+} //Среднее арифметическое массива
+int minimum(int array[], int N)
+{
+	int min = array[0];
+	for (int i = 0; i < N; i++)
+	{
+		if (array[i] < min)
+			min = array[i];
+	}
+	return min;
+}//Минимальное значение массива
+int maximum(int array[], int N)
+{
+	int max = array[0];
+	for (int i = 0; i < N; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return max;
+}//Максимальное значение массива
 void main()
 {
 	srand(time(NULL));
@@ -60,7 +85,10 @@ void main()
 	coutarray(array, N);*/
 	randomfillarray(0, 15, array, N);
 	coutarray(array, N);
-	cout << summelementsofarray(array, N);
+	cout << summelementsofarray(array, N) << endl;
+	cout << middleariphmetical(array, N) << endl;
+	cout << "Minimum: " << minimum(array, N) << endl;
+	cout << "Maximum: " << maximum(array, N) << endl;
 	/*coutarray(array, N);
 	enterarraywithkeyboard(array, N);
 	coutarrayrev(array, N);*/
---
+++ Shift/main.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <conio.h>
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+using namespace std;
+void fillarray(int array[], int N)
+{
+	for (int i = 0; i < N; i++)
+		array[i] = i;
+}//Заполнение массива числами от 0 до N
+void coutarray(int array[], int N)
+{
+	for (int i = 0; i < N; i++)
+		cout << array[i] << " ";
+	cout << endl;
+} //Вывод массива в прямом последовательности
+void shiftarrayleft(int array[], int N, int NumElem)
+{
+	for (int j = 0; j < NumElem; j++)
+	{
+		int tmp = array[0];
+		for (int i = 0; i < N - 1; i++)
+		{
+			array[i] = array[i + 1];
+		}
+		array[N - 1] = tmp;
+	}
+}//Сдвиг массива влево
+void shiftarrayright(int array[], int N, int NumElem)
+{
+	for (int j = 0; j < NumElem; j++)
+	{
+		int tmp = array[N - 1];
+		for (int i = N - 2; i >= 0; i--)
+		{
+			array[i + 1] = array[i];
+		}
+		array[0] = tmp;
+	}
+}//Сдвиг массива вправо
+void coutcellnumber(int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+}//Просто для отображения номеров ячеек.
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int N = 10;
+	int NumElem = 1;
+	int array[N] = {};
+	fillarray(array, N);
+	int key = 0;
+	do
+	{
+		cout << "Нажатие клавиш \"Влево\" или \"Вправо\" двигает массив, \"Вниз\" - завершает работу\n";
+		cout << "Номера ячеек:     "; coutcellnumber(N);
+		cout << "Содержимое ячеек: "; coutarray(array, N);
+		switch (key = _getch())
+		{
+		case LEFT:
+			system("CLS");
+			shiftarrayleft(array, N, NumElem);
+			break;
+		case RIGHT:
+			system("CLS");
+			shiftarrayright(array, N, NumElem);
+			break;
+		case DOWN:
+			key = 0;
+			break;
+		}
+		system("CLS");
+	} while (key);
+}
